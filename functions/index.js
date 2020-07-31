74061cf708617b80e9e8cb961111322d3a71bee8
David Innocent <davidinnocent@live.com>
2020-07-31 09:59:48 +0300

    full working sms with africastalking

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,33 @@
 const functions = require('firebase-functions');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.sms = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+exports.sms = functions.https.onRequest((request, res) => {
+
+    const credentials = {
+        apiKey: 'b7c617e08efbbab9ea9b2c645780dbf20e917f5530a932a9d74ac010f9f0293f', // use your sandbox app API key for development in the test environment
+        username: 'sandbox', // use 'sandbox' for development in the test environment
+    };
+    const africastalking = require('africastalking')(credentials);
+
+    // Initialize a service e.g. SMS
+    const sms = africastalking.SMS
+
+    // Use the service
+    const options = {
+        to: ['+254703127101'],
+        message: "I'm a lumberjack and its ok, I work all night and sleep all day"
+    }
+
+    // Send message and capture the response or error
+    sms.send(options)
+        .then(response => {
+            console.log(response);
+            res.send("Success your message has been sent \n" + JSON.stringify(response.SMSMessageData.Recipients[0]));
+
+        })
+        .catch(error => {
+            console.log(error);
+            res.send("Oops Message not sent \n" + error);
+        });
+
 });
